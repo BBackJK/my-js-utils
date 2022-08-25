d6fce38b139b0fbdb0c1983b3a4ff5338d5d812d
bbacks <wnsrud6839@gmail.com>
2022-08-25 17:42:09 +0900

    date-utils 수정

--- date-utils/date-utils.js
+++ date-utils/date-utils.js
@@ -24,10 +24,10 @@
   const DATE_FORMAT_DOT_YYYYMMDDHHMMSS = 'yyyy.MM.dd hh:mm:ss';
   const DATE_FORMAT_DOT_YYYYMMDDHHMMSS_SSS = 'yyyy.MM.dd hh:mm:ss.SSS';
 
-  const DATE_FORMAT_RDOT_YYYYMMDD = 'yyyy.MM.dd';
-  const DATE_FORMAT_RDOT_YYYYMMDDHHMM = 'yyyy.MM.dd hh:mm';
-  const DATE_FORMAT_RDOT_YYYYMMDDHHMMSS = 'yyyy.MM.dd hh:mm:ss';
-  const DATE_FORMAT_RDOT_YYYYMMDDHHMMSS_SSS = 'yyyy.MM.dd hh:mm:ss.SSS';
+  const DATE_FORMAT_RDOT_YYYYMMDD = 'dd.MM.yyyy';
+  const DATE_FORMAT_RDOT_YYYYMMDDHHMM = 'dd.MM.yyyy hh:mm';
+  const DATE_FORMAT_RDOT_YYYYMMDDHHMMSS = 'dd.MM.yyyy hh:mm:ss';
+  const DATE_FORMAT_RDOT_YYYYMMDDHHMMSS_SSS = 'dd.MM.yyyy hh:mm:ss.SSS';
 
   const DATE_FORMAT_SLASH_YYYYMMDD = 'yyyy/MM/dd';
   const DATE_FORMAT_SLASH_YYYYMMDDHHMM = 'yyyy/MM/dd hh:mm';
@@ -65,7 +65,15 @@
 
   // iso string
   const STR_DATE_FORMAT_ISO_REG = /^(?:\d{4})-(?:\d{2})-(?:\d{2})T(?:\d{2}):(?:\d{2}):(?:\d{2}(?:\.\d*)?)(?:(?:-(?:\d{2}):(?:\d{2})|Z)?)$/;
+  
+  const PER_MILLI_SECONDS = 1000;
+  const PER_SECONDS = PER_MILLI_SECONDS * 60;
+  const PER_MINUTES = PER_SECONDS * 60;
+  const PER_HOUR = PER_MINUTES * 24;
+  const PER_DATE = PER_HOUR * 30;
+  const PER_MONTH = PER_DATE * 12;
 
+  // 객체
   const $DATE = function(value) {
 
     const _this = this;
@@ -76,12 +84,116 @@
     return Object.freeze(_this);
   };
 
-  // $DATE static function
-  // $DATE.
+  /**
+   * argument가 date 타입인지 확인
+   * 예시; console.log($DATE.isDate(new Date())); // true
+   * @param {*} value 
+   */
+  $DATE.isDate = function (value) {
+    return isDate(value);
+  };
+
+  /**
+   * arugment가 this 타입인지 확인
+   * 예시;
+   * const test = new $DATE('2022-08-25);
+   * console.log($DATE.isDATE(test));
+   * @param {*} value 
+   */
+  $DATE.isDATE = function (value) {
+    return value instanceof $DATE;
+  };
+  //endregion
+
+  /**
+   * 두 개의 augument 의 차이를 배열값으로 반환
+   * limitType으로 제한을 둘 수 있음.
+   * 예시;
+   * console.log($DATE.takeGap('2022-08-25 13:00:00', '2022-09-01 12:00', 'date'));
+   * // [0, 0, 6, 23, 0, 0] [년, 월, 일, 시간, 분, 초]
+   * @param {*} val1 date로 변환될 값1
+   * @param {*} val2 date로 변환될 값2
+   * @param {*} limitType 어떤 종류까지 값을 나오게 할 것인지 (['year' (default), 'month', 'date', 'hours', 'minutes', 'seconds'])
+   */
+  $DATE.takeGap = function (val1, val2, limitType) {
+    const date1 = getCompareValueToDate(val1);
+    const date2 = getCompareValueToDate(val2);
+    if (!date1 || !date2) {
+      console.error(' argument value can not convert to Date. ');
+      return [];
+    }
+
+    let distance = date1.getTime() - date2.getTime();
+
+    // 작을 시 강제로 양수화
+    if (distance < 0) {
+      distance *= -1;
+    }
+
+    let gapYear = 0;
+    let gapMonth = 0;
+    let gapDate = 0;
+    let gapHours = 0;
+    let gapMinutes = 0;
+    let gapSeconds = 0;
+
+    switch (limitType) {
+      case 'month':
+        gapMonth = Math.floor(distance/PER_DATE);
+        gapDate = Math.floor((distance % PER_DATE)/PER_HOUR);
+        gapHours = Math.floor((distance % PER_HOUR)/PER_MINUTES);
+        gapMinutes = Math.floor((distance % PER_MINUTES)/PER_SECONDS);
+        gapSeconds = Math.floor((distance % PER_SECONDS)/PER_MILLI_SECONDS);
+        break;
+      case 'date':
+        gapDate = Math.floor(distance/PER_HOUR);
+        gapHours = Math.floor((distance % PER_HOUR)/PER_MINUTES);
+        gapMinutes = Math.floor((distance % PER_MINUTES)/PER_SECONDS);
+        gapSeconds = Math.floor((distance % PER_SECONDS)/PER_MILLI_SECONDS);
+        break;
+      case 'hours':
+        gapHours = Math.floor(distance/PER_MINUTES);
+        gapMinutes = Math.floor((distance % PER_MINUTES)/PER_SECONDS);
+        gapSeconds = Math.floor((distance % PER_SECONDS)/PER_MILLI_SECONDS);
+        break;
+      case 'minutes':
+        gapMinutes = Math.floor(distance/PER_SECONDS);
+        gapSeconds = Math.floor((distance % PER_SECONDS)/PER_MILLI_SECONDS);
+        break;
+      case 'seconds':
+        gapSeconds = Math.floor(distance/PER_MILLI_SECONDS);
+        break;
+      default:
+        gapYear = Math.floor(distance/PER_MONTH);
+        gapMonth = Math.floor((distance % PER_MONTH)/PER_DATE);
+        gapDate = Math.floor((distance % PER_DATE)/PER_HOUR);
+        gapHours = Math.floor((distance % PER_HOUR)/PER_MINUTES);
+        gapMinutes = Math.floor((distance % PER_MINUTES)/PER_SECONDS);
+        gapSeconds = Math.floor((distance % PER_SECONDS)/PER_MILLI_SECONDS);
+        break;
+    }  
+
+    return [gapYear, gapMonth, gapDate, gapHours, gapMinutes, gapSeconds];
+  };
 
   // 외부에서 접근할 수 있는 api 추가
   const fn = $DATE.fn = $DATE.prototype;
 
+  /**
+   * format을 받아서 format형태로 알맞게 변환
+   * 지원 포맷
+   * yyyyMMdd | yyyyMMddhhmm | yyyyMMddhhmmss | yyyyMMddhhmmssSSS
+   * yyyy-MM-dd | yyyy-MM-dd hh:mm | yyyy-MM-dd hh:mm:ss | yyyy-MM-dd hh:mm:ss.SSS
+   * yyyy.MM.dd | yyyy.MM.dd hh:mm | yyyy.MM.dd hh:mm:ss | yyyy.MM.dd hh:mm:ss.SSS
+   * dd.MM.yyyy | dd.MM.yyyy hh:mm | dd.MM.yyyy hh:mm:ss | dd.MM.yyyy hh:mm:ss.SSS
+   * yyyy/MM/dd | yyyy/MM/dd hh:mm | yyyy/MM/dd hh:mm:ss | yyyy/MM/dd hh:mm:ss.SSS
+   * dd/MM/yyyy | dd/MM/yyyy hh:mm | dd/MM/yyyy hh:mm:ss | dd/MM/yyyy hh:mm:ss.SSS
+   * 예시;
+   * const test = new $DATE('2022-08-25');
+   * console.log(test.toString('yyyyMMddhhmm')); // 202208250000
+   * console.log(test.toString('yyyy-MM-dd hh:mm')); // 2022-08-25 00:00
+   * @param {*} format 
+   */
   fn.toString = function (format) {
 
     let result = '';
@@ -183,8 +295,197 @@
     return result;
   };
 
-  fn.isDate = function (value) {
-    return isDate(value);
+  /**
+   * argument 값 만큼 year을 계산
+   * 예시;
+   * const test = new $DATE('2022-08-25');
+   * const afterTwoYearDATE = test.addYear(2);
+   * console.log(afterTwoYearDATE.date); // Sun Aug 25 2024 00:00:00 GMT+0900 (한국 표준시)
+   * console.log(afterTwoYearDATE.toString('yyyy.MM.dd')); // 2024.08.25
+   * @param {*} year 
+   */
+  fn.addYear = function (year) {
+    let arr = getDateTimeArr(this.date);
+    if (arr.length !== 7) {
+      return null;
+    }
+
+    const newDate = new Date(
+      Number(arr[0]) + year
+      , Number(arr[1]) - 1
+      , Number(arr[2])
+      , Number(arr[3])
+      , Number(arr[4])
+      , Number(arr[5])
+      , Number(arr[6])
+    );
+
+    return new $DATE(newDate);
+  };
+
+  /**
+   * argument 값 만큼 month 을 계산
+   * 예시;
+   * const test = new $DATE('2022-08-25');
+   * const afterTwoMonthDATE = test.addMonth(2);
+   * console.log(afterTwoMonthDATE.date); // Tue Oct 25 2022 00:00:00 GMT+0900 (한국 표준시)
+   * console.log(afterTwoMonthDATE.toString('yyyy.MM.dd')); 2022.10.25
+   * @param {*} month 
+   */
+  fn.addMonth = function (month) {
+    let arr = getDateTimeArr(this.date);
+    if (arr.length !== 7) {
+      return null;
+    }
+
+    const newDate = new Date(
+      Number(arr[0])
+      , Number(arr[1]) - 1 + month
+      , Number(arr[2])
+      , Number(arr[3])
+      , Number(arr[4])
+      , Number(arr[5])
+      , Number(arr[6])
+    );
+
+    return new $DATE(newDate);
+  };
+
+  /**
+   * argument 값 만큼 date 을 계산
+   * 예시;
+   * const test = new $DATE('2022-08-25');
+   * const test2 = test.addDate(100);
+   * console.log(test2.date); // Sat Dec 03 2022 00:00:00 GMT+0900 (한국 표준시)
+   * console.log(test2.toString('yyyy.MM.dd')); // 2022.12.03
+   * @param {*} date 
+   */
+  fn.addDate = function (date) {
+    let arr = getDateTimeArr(this.date);
+    if (arr.length !== 7) {
+      return null;
+    }
+
+    const newDate = new Date(
+      Number(arr[0])
+      , Number(arr[1]) - 1
+      , Number(arr[2]) + date
+      , Number(arr[3])
+      , Number(arr[4])
+      , Number(arr[5])
+      , Number(arr[6])
+    );
+
+    return new $DATE(newDate);
+  };
+
+  /**
+   * 깊은 복사
+   */
+  fn.deepCopy = function () {
+    return new $DATE(this.input);
+  };
+
+  /**
+   * this 보다 argument (value)가 미래 인지 판단
+   * 예시;
+   * const test = new $DATE('2022-08-25');
+   * const test2 = test.addDate(100);
+   * console.log(test2.isAfter(test)); // true
+   * console.log(test.isAfter(test2)); // false
+   * @param {*} value 
+   */
+  fn.isAfter = function (value) {
+    if (!value) {
+      return false;
+    }
+
+    let compareDate = getCompareValueToDate(value);
+    console.log(compareDate);
+    if (!isDate(compareDate)) {
+      return false;
+    }
+
+    return compareDate.getTime() < this.date.getTime();
+  };
+
+   /**
+   * this 보다 argument (value)가 과거 인지 판단
+   * 예시;
+   * const test = new $DATE('2022-08-25');
+   * const test2 = test.addDate(100);
+   * console.log(test2.isBefore(test)); // false
+   * console.log(test.isBefore(test2)); // true
+   * @param {*} value 
+   */
+  fn.isBefore = function (value) {
+    if (!value) {
+      return false;
+    }
+
+    let compareDate = getCompareValueToDate(value);
+    if (!isDate(compareDate)) {
+      return false;
+    }
+
+    return compareDate.getTime() > this.date.getTime();
+  };
+
+  /**
+   * this 보다 argument (value)가 같은지 판단
+   * 예시;
+   * const test = new $DATE('2022-08-25');
+   * const test2 = test.deepCopy();
+   * console.log(test.isSame(test2));
+   * @param {*} value 
+   */
+  fn.isSame = function (value) {
+    if (!value) {
+      return false;
+    }
+
+    let compareDate = getCompareValueToDate(value);
+    if (!isDate(compareDate)) {
+      return false;
+    }
+
+    return compareDate.getTime() === this.date.getTime();
+  };
+
+  /**
+   * 객체의 시작일 구하기
+   * 예시;
+   * const test = new $DATE('2022-08-25');
+   * const sTest = test.takeStart();
+   * console.log(sTest.date); // Mon Aug 01 2022 00:00:00 GMT+0900 (한국 표준시)
+   * console.log(sTest.toString('yyyy-mm-dd hh:mm')); // 2022-08-01 00:00
+   */
+  fn.takeStart = function () {
+    let arr = getDateArr(this.date);
+
+    if (arr.length !== 3) {
+      return null;
+    }
+
+    return new $DATE(new Date(arr[0], arr[1] - 1, 1));
+  };
+
+  /**
+   * 객체의 마지막일 구하기
+   * 예시;
+   * const test = new $DATE('2022-08-25');
+   * const eTest = test.takeEnd();
+   * console.log(eTest.date); // Wed Aug 31 2022 00:00:00 GMT+0900 (한국 표준시)
+   * console.log(eTest.toString('yyyy-mm-dd hh:mm')); // 2022-08-31 00:00
+   */
+  fn.takeEnd = function () {
+    let arr = getDateArr(this.date);
+
+    if (arr.length !== 3) {
+      return null;
+    }
+
+    return new $DATE(new Date(arr[0], arr[1], 0));
   };
 
   const getHourMinutesString = function (date) {
@@ -272,6 +573,10 @@
     return value && Object.prototype.toString.call(value) === DATE_TYPE_STRING && !isNaN(value);
   };
 
+  const isString = function (value) {
+    return typeof value === 'string';
+  };
+
   const zp = function (value, count) {
     if (value === null || value === undefined) {
       return '';
@@ -294,6 +599,22 @@
     }
 
     return zero + val;
+  };
+
+  const getCompareValueToDate = function (value) {
+    let compareDate = null;
+
+    if (isString(value)) {
+      compareDate = toDate(value);
+    } else if (isDate(value)) {
+      compareDate = value;
+    } else if ($DATE.isDATE(value)) {
+      compareDate = new Date(value.date.getTime());
+    } else {
+      compareDate = new Date(value);
+    }
+
+    return compareDate;
   };
 
   window.$DATE = $DATE;
